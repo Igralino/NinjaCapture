8f4c0736decbb296bd700b05065423bbf86ea9ce
Igor Yunash <igralino@ya.ru>
2018-01-18 16:54:02 +0300

    First commit

---
+++ NinjaCapture/NinjaCapture/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NinjaCapture
+//
+//  Created by Igor Yunash on 18.01.18.
+//  Copyright © 2018 Igralino. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NinjaCapture/NinjaCapture/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NinjaCapture
+//
+//  Created by Igor Yunash on 18.01.18.
+//  Copyright © 2018 Igralino. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
